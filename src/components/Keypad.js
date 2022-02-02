991310348328db324356ba0655917de3680e9aad
Matt Quinn <mjq0533@gmail.com>
2022-02-02 10:06:36 -0600

    complete

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+
+function KeyPad() {
+
+    function handleChange(){
+        console.log('Entering password...')
+    }
+
+    return (
+        <input type = "password" onChange = {handleChange} />
+    )
+}
+
+export default KeyPad
